1c224196fbb0616a2b50512157f2511b40a9754d
GarboMuffin <tommyweber33@gmail.com>
2019-01-31 16:57:07 -0600

    fix **really** bad firefox performance issue
    Apparently Firefox would repaint a lot more than other browsers when the
    timestamp changes. This makes a few minor changes that make Firefox be
    smarter about repainting only what it needs to.
    Brings FPS from ~44 to ~60 on my computer. Quite significant.

--- src/components/layers/LayerMeta.vue
+++ src/components/layers/LayerMeta.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="row layer flex flex-row flex-center">
-    <input type="text" v-model="layer.name" :placeholder="layer.placeholder" name="name">
-    <input type="number" v-model="volume" name="volume" class="no-spinners">
-    <!-- Janky ['delete'] allows it to call delete() without using the word delete because vue errors if I do that -->
-    <a @click="layer['delete']()">&times;</a>
+  <div class="row layer">
+    <div class="child">
+      <input type="text" v-model="layer.name" :placeholder="layer.placeholder" name="name">
+      <input type="number" v-model="volume" name="volume" class="no-spinners">
+      <!-- Janky ['delete'] allows it to call delete() without using the word delete because vue errors if I do that -->
+      <a @click="layer['delete']()">&times;</a>
+    </div>
   </div>
 </template>
 
@@ -28,6 +30,14 @@ export default {
 </script>
 
 <style scoped>
+.layer {
+  position: relative;
+}
+.child {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
 input {
   font-size: 11px;
 }
